84303e4e5ed3eab3662287c83834224dac1a00bd
Josue Arce <josue.arce@webmechanix.com>
2019-04-30 22:19:44 -0400

    Adding registries site to portfolio

---
+++ src/content/projects/NIH RaDaR/radar.md
@@ -0,0 +1,12 @@
+---
+name: 'National Institutes of Health | RaDaR'
+description: 'Rare Diseases Registry Program Website'
+image: './registries.ncats.nih.gov_module_setting-up-your-registry_create-your-registry-plan_.png'
+url: 'https://careers.choicehotels.com'
+tech:
+    - HTML
+    - SCSS
+    - JavaScript
+    - PHP
+    - WordPress
+---
--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export const query = graphql`
       }
     }
   }
-  projects: allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/projects/" } }, sort: { fields: [frontmatter___name], order: ASC }) {
+  projects: allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/projects/" } }, sort: { fields: id, order: ASC }) {
     edges {
       node {
         frontmatter {
